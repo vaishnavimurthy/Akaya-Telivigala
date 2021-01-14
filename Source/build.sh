a89728e51d18dd2e5554808477eb4921be1a41b9
Yanone <post@yanone.de>
2021-01-14 16:19:25 +0500

    Ready for GF

---
+++ Source/build.sh
@@ -0,0 +1,14 @@
+TTF="TTF/AkayaTelivigala-Regular.ttf"
+
+# Delete olf font to make sure we’re looking at a freshly generated font after here
+rm -f $TTF
+
+# Generate
+fontmake -g Source/AkayaTelivigala.glyphs -a -o ttf --output-path $TTF
+
+gftools fix-hinting $TTF
+mv $TTF.fix $TTF
+gftools fix-dsig $TTF -a -f
+
+rm -r instance_ufo
+rm -r master_ufo
